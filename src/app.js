bb8c313e94fa91a7967d4a603e2806827759cb2c
Farhan Sadik <farhansadik0760@gmail.com>
2025-09-08 02:27:47 +0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   public/assets/index-DKL4aErq.js
    	new file:   public/assets/index-D_p6vie9.css
    	new file:   public/index.html
    	new file:   public/vite.svg
    	modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 app.use(require("express").json());
 app.use(
@@ -9,11 +10,14 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/chat", require("./routes/chat.routes"));
 app.use("/api/messages", require("./routes/message.routes"));
-
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
